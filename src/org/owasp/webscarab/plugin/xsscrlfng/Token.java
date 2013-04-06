4f6cfea8bf382105c9643d7936b586121278abda
Jérémy Lebourdais <jeremy.lebourdais@free.fr>
2013-04-06 03:07:04 +0200

    New Token class to hold information about a value to test
    It holds the type of the token, its value, an id to identify it in the
    session and an optional index if it is part of a sequence.

--- src/org/owasp/webscarab/plugin/xsscrlfng/Token.java
+++ src/org/owasp/webscarab/plugin/xsscrlfng/Token.java
@@ -1,18 +1,39 @@
 package org.owasp.webscarab.plugin.xsscrlfng;
 
+/** Represents a token, which consists of:
+ * a type of a test (XSS, SQLi-Num, SQLi-Text, etc),
+ * a generated value,
+ * an Id (int value incremented for each new Token, cf. TokenGenerator)
+ * and an optional index, which is used when a test needs more than one token (typically the SQL injection tokens).
+ *  
+ * @author Jérémy Lebourdais
+ * @see TokenGenerator
+ *
+ */
 public class Token {
 	
+	// Type of the token, like XSS-Test, SQLi-Num, SQLi-Test, etc
 	private String tokenType = "";
+	// Value generated for this token
 	private String tokenValue = "";
-	private String tokenIndex = "";
+	// Id of this token, should be unique in one session
+	private int tokenId = 0;
+	// Index of this token if it is used in a subset, like for SQLi tests where 8 values are used for one test
+	private int tokenTestIndex = 0;
 	
-	
-	public Token(final String tokenType, final String tokenValue, final String tokenIndex) {
+	/** Creates a new instance.
+	 * @param tokenType type of a test (XSS, SQLi-Num, SQLi-Text, etc),
+	 * @param tokenValue generated value (the one which will be injected)
+	 * @param tokenId Id of the token in the session (int value incremented for each new Token, cf. TokenGenerator)
+	 * @param tokenIndex optional index (0 if not used), which is used when a test needs more than one token (typically the SQL injection tokens).
+	 */
+	public Token(final String tokenType, final String tokenValue, final int tokenId, final int tokenIndex) {
 		this.tokenType = tokenType;
 		this.tokenValue = tokenValue;
-		this.tokenIndex = tokenIndex;
+		this.tokenId = tokenId;
+		this.tokenTestIndex = tokenIndex;
 	}
-	
+
 	public String getTokenType() {
 		return tokenType;
 	}
@@ -25,20 +46,32 @@ public class Token {
 	public void setTokenValue(final String tokenValue) {
 		this.tokenValue = tokenValue;
 	}
-	public String getTokenIndex() {
-		return tokenIndex;
+	public int getTokenId() {
+		return tokenId;
 	}
-	public void setTokenIndex(final String tokenIndex) {
-		this.tokenIndex = tokenIndex;
+	public void setTokenId(final int newValue) {
+		if (newValue > 0) {
+			this.tokenId = newValue;
+		}
 	}
 	
 	public Boolean hasIndex()
 	{
-		Boolean indexFound = Boolean.FALSE;
-		if (! "".equals(tokenIndex)) {
-			indexFound = Boolean.TRUE;
+		return (tokenTestIndex > 0);
+	}
+
+	public int getTokenTestIndex() {
+		return tokenTestIndex;
+	}
+
+	public void setTokenTestIndex(int newValue) {
+		if (newValue > 0) {
+			this.tokenTestIndex = newValue;
 		}
-		return indexFound;
+	}
+	
+	public String toString() {
+		return tokenType + ":" + tokenId + ":" + tokenTestIndex + ":" + tokenValue;
 	}
 	 
 }
