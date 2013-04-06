c4b68ba9f2626016679d8d7a78872607e79bf01a
Jérémy Lebourdais <jeremy.lebourdais@free.fr>
2013-04-06 03:12:17 +0200

    New class TokenGenerator to dissociate the generation from the tests.
    This class generates a list of tokens to be used for tests. It uses an
    unique Id (an int) for each generated token during one session, to
    identify it easily during the tests.

--- src/org/owasp/webscarab/plugin/xsscrlfng/TokenGenerator.java
+++ src/org/owasp/webscarab/plugin/xsscrlfng/TokenGenerator.java
@@ -2,6 +2,7 @@ package org.owasp.webscarab.plugin.xsscrlfng;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.logging.Logger;
 
@@ -10,26 +11,15 @@ public class TokenGenerator {
 	// ID used to generate parameters for tests, in order to have an unique identifier for each test
 	private static AtomicInteger uniqueId  = new AtomicInteger();
 
+	// TODO Create a dedicated Configuration class to hold configuration values.
 	// Model used to get initialization values
-	private final XSSCRLFngModel model;
+	private final ConfigurationHolder configuration;
 
 	private final static Logger LOGGER = Logger.getLogger(TokenGenerator.class.getName());
 
-	// Character used to generate long values
-	private final static char CHAR_TO_REPEAT = 'A';
 	
-	private final static String PATTERN_PREFIX = "EDW-";
-	private final static String PATTERN_SUFFIX = "_EDW";
-	
-	private final static String SQLI_TOKENS[][] = {{ " AND 1=2", " AND 2=3", " AND0O", " OR0O"," AND 4=3", " AND 5=5", " OR 7=7", " OR 9=9 OR 10=11"},
-														{ "''", "''''", "'", "'''","' AND 'A1'='B2", "' AND 'C3'='C3", "' OR 'D4'='D4", "' OR 'F5'='F5' OR 'G6'='H7"}};
-	// Perform or not SQLi tests
-	private Boolean performSQLiTests = Boolean.FALSE;
-
-	
-	public TokenGenerator(final XSSCRLFngModel model, final Boolean performSQLiTests) {
-		this.model = model;
-		this.performSQLiTests = performSQLiTests;
+	public TokenGenerator(final ConfigurationHolder configuration) {
+		this.configuration = configuration;
 	}
 
 	/** Generates an array of crafted values to be used to test a parameter.
@@ -45,36 +35,48 @@ public class TokenGenerator {
 		}
 		
 		// Generate tokens for XSS
-		final String xssTokens[] = model.getXSSTestStrings().split("\\r?\\n");
+		final String xssTokens[] = configuration.getXssTestStrings().split("\\r?\\n");
 		LOGGER.fine("xssTokens[]: " + Arrays.toString(xssTokens));
 		
 		Token tokenGenerated = null;
 		final ArrayList<Token> newTestTokens = new ArrayList<Token>();
+		int tokenId = 0;
 		
+		// TODO: transform this with the TokenConfigurator
 		for (int i=0; i<xssTokens.length;i++) {
-			final String tokenValue = PATTERN_PREFIX + uniqueId.addAndGet(1) + xssTokens[i] + PATTERN_SUFFIX;
-			tokenGenerated = new Token("XSS-Test", tokenValue, ""); // NOPMD by Jérémy Lebourdais on 05/04/13 20:57
+			tokenId = uniqueId.addAndGet(1);
+			final String tokenValue = configuration.getTokenPrefix() + tokenId + xssTokens[i] + configuration.getTokenSuffix();
+			tokenGenerated = new Token("XSS-Test", tokenValue, tokenId, 0); // NOPMD by Jérémy Lebourdais on 05/04/13 20:57
 			newTestTokens.add(tokenGenerated);
 			LOGGER.fine("Token added for XSS: " + tokenGenerated.getTokenValue());
 		}
 		
 		// Generate LongValue tokens
 		final char[] chars = new char[5000];
-		Arrays.fill(chars, CHAR_TO_REPEAT);
-		
-		final String newLongToken = PATTERN_PREFIX + uniqueId.addAndGet(1) + new String(chars) + PATTERN_SUFFIX;
-		tokenGenerated = new Token("LongValue", newLongToken, "");
+		Arrays.fill(chars, configuration.getTokenCharToRepeat());
+		tokenId = uniqueId.addAndGet(1);
+		final String newLongTokenValue = configuration.getTokenPrefix() + tokenId + new String(chars) + configuration.getTokenSuffix();
+		tokenGenerated = new Token("LongValue", newLongTokenValue, tokenId, 0);
 		newTestTokens.add(tokenGenerated);
 		LOGGER.fine("Token added for LongValue: " + tokenGenerated.getTokenValue());
 		
 		// If needed, generate tokens for SQLi
-		if (performSQLiTests) {
+		if (configuration.getPerformSQLiTests()) {
+			final Iterator<TokenConfigurator> confIterator = configuration.getSqliConfigurators().iterator();
 			
-			final String sqliTokensType[] = {"SQLi-Num-" + uniqueId.addAndGet(1), "SQLi-Text-" + uniqueId.addAndGet(1)};
-			
-			for (int i=0; i<sqliTokensType.length; i++) {
-				for (int j=0; j<SQLI_TOKENS[0].length;j++) {
-					tokenGenerated = new Token(sqliTokensType[i], baseParameter + SQLI_TOKENS[i][j], Integer.toString(j)); // NOPMD by Jérémy Lebourdais on 05/04/13 20:57
+			while (confIterator.hasNext()) {
+				// Id of the generated SQL Test
+				tokenId = uniqueId.addAndGet(1);
+				// Get the TokenConfigurator
+				final TokenConfigurator currentTokenConfigurator = confIterator.next();
+				
+				// Retrieve associated values and create tokens
+				final Iterator<String> sqliValueIterator = currentTokenConfigurator.getTokenValues().iterator();
+				int index = 0;
+				while (sqliValueIterator.hasNext()) {
+					index++;
+					
+					tokenGenerated = new Token(currentTokenConfigurator.getTokenTestName(), baseParameter + sqliValueIterator.next(), tokenId, index); // NOPMD by Jérémy Lebourdais on 05/04/13 20:57
 					newTestTokens.add(tokenGenerated);
 					LOGGER.fine("Token added for SQLi: "+tokenGenerated.getTokenValue());
 				}
@@ -83,11 +85,4 @@ public class TokenGenerator {
 		return newTestTokens;
 	}
 	
-	public Boolean getPerformSQLiTests() {
-		return performSQLiTests;
-	}
-
-	public void setPerformSQLiTests(final Boolean performSQLiTests) {
-		this.performSQLiTests = performSQLiTests;
-	}
 }
